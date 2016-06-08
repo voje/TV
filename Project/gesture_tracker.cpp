4b852cb9ee77514c913551e1ac3acaff330d137e
voje <kristjan.voje@gmail.com>
2016-06-08 14:37:03 +0200

    todo: define states

--- Project/gesture_tracker.cpp
+++ Project/gesture_tracker.cpp
@@ -19,11 +19,111 @@ Height of area [2 5]: rest of frame.
 
 GestureTracker::GestureTracker(int delay){
 	this->running = false;
-	this->l_hand = 2;
-	this->r_hand = 5;
+	this->head_old_y = -1;
+	this->head_percent = 0.02;
+	this->head_running_vote = 0;
+	this->head_standing_vote = 0;
+	this->l_hand = -1;
+	this->l_hand_vote = 0;
+	this->r_hand = -1;
+	this->r_hand_vote = 0;
 	this->delay = delay;
 }
 
-void GestureTracker::update(Rect face_rect, Point l_hand, Point r_hand){
+void GestureTracker::update(Rect face_rect, Point l_hand, Point r_hand, Mat &frame){
 
+	//update head
+	//set a margin for head bobbing, relative to frame height. say 2% or something (test it)
+	int y_diff = floor(frame.rows * head_percent);
+	int head_y = face_rect.tl().y;
+	//if not initialized
+	if(head_old_y == -1){
+		head_old_y = head_y;
+		return;
+	}
+	int dy = abs(head_old_y - head_y);
+	if(!running){
+		if(dy > y_diff){
+			head_running_vote += 1;
+		}else{
+			head_running_vote = 0;
+		}
+	}else if(running){
+		if( dy > y_diff){
+			head_standing_vote = 0;
+		}else{
+			head_standing_vote +=1;
+		}
+	}
+	if(head_running_vote >= delay){
+		running = true;
+		head_running_vote = 0;
+	}
+	if(head_standing_vote >= delay){
+		running = false;
+		head_standing_vote = 0;
+	}
+
+	int mid_height = floor(0.2*frame.rows);
+
+	//update l_hand
+	int ly = l_hand.y;
+	int l_hand_new;
+	if(ly < head_y){
+		l_hand_new = 1;
+	}else if(ly < head_y + mid_height){
+		l_hand_new = 2;
+	}else{
+		l_hand_new = 3;
+	}
+
+	if(l_hand_new != this->l_hand){
+		l_hand_vote += 1;
+	}else{
+		l_hand_vote = 0;
+	}
+
+	if(l_hand_vote >= delay){
+		this->l_hand = l_hand_new;
+		l_hand_vote = 0;
+	}
+
+	//update r_hand
+	int ry = r_hand.y;
+	int r_hand_new;
+	if(ry < head_y){
+		r_hand_new = 4;
+	}else if(ry < head_y + mid_height){
+		r_hand_new = 5;	
+	}else{
+		r_hand_new = 6;
+	}
+
+	if(r_hand_new != this->r_hand){
+		r_hand_vote += 1;
+	}else{
+		r_hand_vote = 0;
+	}
+
+	if(r_hand_vote >= delay){
+		this->r_hand = r_hand_new;
+		r_hand_vote = 0;
+	}
+
+}//update
+
+int GestureTracker::get_state(){
+	if(l_hand == -1 || r_hand == -1){
+		return -1;
+	}	
+	//todo: write all possible states
 }
+
+
+
+
+
+
+
+
+
--- Project/gesture_tracker.h
+++ Project/gesture_tracker.h
@@ -5,12 +5,19 @@
 
 class GestureTracker{
 	bool running;
+	int head_old_y;
+	double head_percent;
+	int head_running_vote;
+	int head_standing_vote;
 	int l_hand;
+	int l_hand_vote;
 	int r_hand;
+	int r_hand_vote;
 	int delay;
 public:
 	GestureTracker(int);	
-	void update(cv::Rect, cv::Point, cv::Point);
+	void update(cv::Rect, cv::Point, cv::Point, cv::Mat&);
+	int get_state();
 };
 
 #endif
